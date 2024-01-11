06c65da3823cd510c479f99e20a24d67765c4034
Peter Pan <pantao@particle.network>
2024-01-11 18:43:36 +0800

    Add new chain Ancient8

--- scripts/deploy-aa.ts
+++ scripts/deploy-aa.ts
@@ -1,4 +1,4 @@
-// npx ts-node scripts/deploy-aa.ts privateKey 11155111
+// npx ts-node scripts/deploy-aa.ts privateKey 11155111 false
 import { Wallet } from 'ethers';
 import { deployDetermineDeployer } from './deploy-determine-deployer';
 import { deploySimpleAccountFactory } from './deploy-simple-account-factory';
@@ -9,6 +9,7 @@ import { deployBTCAccountFactory } from './deploy-btc-account-factory';
 const args = process.argv.slice(2);
 const privateKey = args[0];
 const chainId = args[1] ? parseInt(args[1]) : 5;
+const deployBTCAccount = args[2] ? args[2] === 'true' : false;
 
 (async () => {
     const signer = new Wallet(privateKey);
@@ -17,5 +18,7 @@ const chainId = args[1] ? parseInt(args[1]) : 5;
     await deployDetermineDeployer(chainId, signer);
     await deploySimpleAccountFactory(chainId, signer);
     await deployEntryPoint(chainId, signer);
-    await deployBTCAccountFactory(chainId, signer);
+    if (deployBTCAccount) {
+        await deployBTCAccountFactory(chainId, signer);
+    }
 })();
