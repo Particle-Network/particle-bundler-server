cfc4de92803d7770c942291db3dfb774436db4b8
Peter Pan <pantao@particle.network>
2024-01-17 14:32:43 +0800

    Fix rpc batch issue

--- scripts/deploy-determine-deployer.ts
+++ scripts/deploy-determine-deployer.ts
@@ -1,12 +1,16 @@
-import { JsonRpcProvider, Wallet, parseEther } from 'ethers';
+import { FetchRequest, JsonRpcProvider, Network, Wallet, parseEther } from 'ethers';
 import { RPC_CONFIG } from '../src/configs/bundler-common';
+import { PROVIDER_FETCH_TIMEOUT } from '../src/common/common-types';
 
 const contractAddress = '0x4e59b44847b379578588920ca78fbf26c0b4956c';
 const factoryDeployer = '0x3fab184622dc19b6109349b94811493bf2a45362';
 
 export const deployDetermineDeployer = async (chainId: number, signer: Wallet) => {
     const rpcUrl = RPC_CONFIG[String(chainId)].rpcUrl;
-    const provider = new JsonRpcProvider(rpcUrl);
+    const network = new Network('', chainId);
+    const fetchRequest = new FetchRequest(rpcUrl);
+    fetchRequest.timeout = PROVIDER_FETCH_TIMEOUT;
+    const provider = new JsonRpcProvider(fetchRequest, network, { batchMaxCount: 1, staticNetwork: network });
     signer = signer.connect(provider);
 
     const code = await provider.getCode(contractAddress);
@@ -30,6 +34,6 @@ export const deployDetermineDeployer = async (chainId: number, signer: Wallet) =
         '0xf8a58085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf31ba02222222222222222222222222222222222222222222222222222222222222222a02222222222222222222222222222222222222222222222222222222222222222';
 
     const r = await provider.send('eth_sendRawTransaction', [rawTransaction]);
-    
+
     console.log('send tx deploy', r);
 };
