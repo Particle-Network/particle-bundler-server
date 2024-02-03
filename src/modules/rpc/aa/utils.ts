7264c03c52a8754805fe637d23e32060452f2168
Peter Pan <pantao@particle.network>
2024-02-03 15:06:24 +0800

    Set to 0.05 Gwei

--- src/modules/rpc/aa/utils.ts
+++ src/modules/rpc/aa/utils.ts
@@ -97,7 +97,16 @@ export async function getFeeDataFromParticle(chainId: number, level: string = GA
         };
     }
 
-    if ([EVM_CHAIN_ID.MERLIN_CHAIN_MAINNET, EVM_CHAIN_ID.MERLIN_CHAIN_TESTNET].includes(chainId)) {
+    if ([EVM_CHAIN_ID.MERLIN_CHAIN_MAINNET].includes(chainId)) {
+        return {
+            maxPriorityFeePerGas: 50000000,
+            maxFeePerGas: 50000000,
+            gasPrice: 50000000,
+            baseFee: 0,
+        };
+    }
+
+    if ([EVM_CHAIN_ID.MERLIN_CHAIN_TESTNET].includes(chainId)) {
         return {
             maxPriorityFeePerGas: 100000000,
             maxFeePerGas: 100000000,
