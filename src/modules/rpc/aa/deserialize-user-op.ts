e01ce09b96044a7308d0325e0af9354330c8951c
GDdark <huangdongxc@gmail.com>
2024-02-28 18:33:50 +0800

    Detect errors

---
+++ src/modules/rpc/aa/deserialize-user-op.ts
@@ -0,0 +1,70 @@
+import { Interface } from 'ethers';
+import { BigNumber } from '../../../common/bignumber';
+
+interface Itx {
+    to: string;
+    value: bigint;
+    data: string;
+}
+
+export function deserializeUserOpCalldata(callData: string): Itx[] {
+    const txs: Itx[] = [];
+
+    const abis: { abi: any[]; batch: boolean }[] = [
+        { abi: ['function execute(address dest, uint256 value, bytes calldata func) external'], batch: false },
+        { abi: ['function execute(address to, uint256 value, bytes calldata data, uint8 operation) external'], batch: false },
+        { abi: ['function executeBatch(address[] calldata dest, bytes[] calldata func) external'], batch: true },
+        { abi: ['function executeBatch(address[] calldata dest, uint256[] calldata value, bytes[] calldata func) external'], batch: true },
+        { abi: ['function execute_ncC(address dest, uint256 value, bytes calldata func) public'], batch: false },
+        { abi: ['function executeBatch_y6U(address[] calldata dest, uint256[] calldata value, bytes[] calldata func) public'], batch: true },
+    ];
+
+    for (const item of abis) {
+        const iface = new Interface(item.abi);
+
+        const functionFragment = iface.getFunction(item.abi[0]);
+        if (!callData.startsWith(functionFragment.selector)) {
+            continue;
+        }
+
+        const decoded = iface.decodeFunctionData(item.abi[0], callData);
+        if (item.batch) {
+            if (decoded.length === 2) {
+                const dests = decoded[0];
+                const funcs = decoded[1];
+                for (let i = 0; i < dests.length; i++) {
+                    txs.push({
+                        to: dests[i],
+                        value: 0n,
+                        data: funcs[i],
+                    });
+                }
+            }
+
+            if (decoded.length === 3) {
+                const dests = decoded[0];
+                const values = decoded[1];
+                const funcs = decoded[2];
+                for (let i = 0; i < dests.length; i++) {
+                    txs.push({
+                        to: dests[i],
+                        value: BigNumber.from(values[i]).toBigInt(),
+                        data: funcs[i],
+                    });
+                }
+            }
+        } else {
+            if (decoded.length >= 3) {
+                txs.push({
+                    to: decoded[0],
+                    value: BigNumber.from(decoded[1]).toBigInt(),
+                    data: decoded[2],
+                });
+            }
+        }
+
+        break;
+    }
+
+    return txs;
+}
